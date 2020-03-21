a7f679c547fb42b19302e17b587d442e4d7674e7
tingsmar <martin.burheim.tingsta@visma.com>
2020-03-21 19:55:14 +0100

    Initial commit

--- MMM-Tesla.js
+++ MMM-Tesla.js
@@ -3,7 +3,7 @@
 /* Magic Mirror
  * Module: MMM-Tesla
  *
- * By Jan Henrik Gundelsby
+ * By Jan Henrik Gundelsby and Martin Burheim Tingstad
  * MIT Licensed.
  */
 
@@ -17,7 +17,6 @@ Module.register("MMM-Tesla",{
 
 		initialLoadDelay: 0, // 0 seconds delay
 		retryDelay: 2500,
-
 	},
 	
 	// Define required scripts.
@@ -54,12 +53,88 @@ Module.register("MMM-Tesla",{
     	iframe.width="200";
     	iframe.height="150";
     	iframe.style="border:0";
-    	wrapper.appendChild(iframe);
+    	//wrapper.appendChild(iframe);
 
 		var textElement = document.createElement("div");
-       	textElement.innerHTML = '<b>Tesla</b><br/>Charging state: ' + this.charging_state + ' - Battery level: ' + 
-			this.battery_level + '% - ' + 'Drive state: ' + this.shift_state;
-       	wrapper.appendChild(textElement);		
+		   textElement.innerHTML = '<b>Tesla</b><br/>' +
+		    this.charging_state + ' - ' + Math.floor(this.range) + ' km';
+
+		wrapper.appendChild(textElement);
+
+		var svgNS = "http://www.w3.org/2000/svg";
+		var svg = document.createElementNS(svgNS, "svg");
+		svg.setAttribute('width', 212);
+		svg.setAttribute('height', 60);
+
+		var batteryFrame = document.createElementNS(svgNS, "rect");
+
+		batteryFrame.setAttribute('width', 206);
+	    batteryFrame.setAttribute('height', 58);
+		batteryFrame.setAttribute('style', "fill:rgba(0,0,0,0);stroke-width:2;stroke:rgba(255,255,255, 0.75)");
+		batteryFrame.setAttribute("rx", 5);
+		batteryFrame.setAttribute("ry", 5);
+		svg.appendChild(batteryFrame);
+
+		var shiftedContentContainer = document.createElementNS(svgNS, "svg");
+		shiftedContentContainer.setAttribute("x", "3");
+		shiftedContentContainer.setAttribute("y", "3");
+	    //shiftedContentContainer.setAttribute("transform", "translate(3,3)");
+
+		var batteryContent = document.createElementNS(svgNS, "rect");
+
+	    batteryContent.setAttribute('width', this.battery_level*2);
+	    batteryContent.setAttribute('height', 52);
+		batteryContent.setAttribute('style', "fill:rgba(45,220,45,0.7)");
+		batteryContent.setAttribute("rx", 1);
+		batteryContent.setAttribute("ry", 1);
+		shiftedContentContainer.appendChild(batteryContent);
+
+		var chargeLevelText = document.createElementNS(svgNS, "text");
+		chargeLevelText.setAttribute("x", "25");
+		chargeLevelText.setAttribute("y", "36");
+		chargeLevelText.setAttribute("style", "fill:rgba(255,255,255,0.4); font: bold 30px sans-serif;");
+
+		var textNode = document.createTextNode(this.battery_level + '%');
+		chargeLevelText.appendChild(textNode);
+		shiftedContentContainer.appendChild(chargeLevelText);
+
+		var batteryBar = document.createElementNS(svgNS, "path");
+		batteryBar.setAttribute("stroke", "#ffffff");
+		batteryBar.setAttribute("d", "M100 0 L100 52");
+		batteryBar.setAttribute('stroke-width', "2");
+		batteryBar.setAttribute('opacity', "0.25");
+		shiftedContentContainer.appendChild(batteryBar);
+
+		var batteryBar = document.createElementNS(svgNS, "path");
+		batteryBar.setAttribute("stroke", "#ffffff");
+		batteryBar.setAttribute("d", "M120 0 L120 52");
+		batteryBar.setAttribute('stroke-width', "2");
+		batteryBar.setAttribute('opacity', "0.25");
+		shiftedContentContainer.appendChild(batteryBar);
+
+		var batteryBar = document.createElementNS(svgNS, "path");
+		batteryBar.setAttribute("stroke", "#ffffff");
+		batteryBar.setAttribute("d", "M140 0 L140 52");
+		batteryBar.setAttribute('stroke-width', "2");
+		batteryBar.setAttribute('opacity', "0.25");
+		shiftedContentContainer.appendChild(batteryBar);
+
+		var batteryBar = document.createElementNS(svgNS, "path");
+		batteryBar.setAttribute("stroke", "#ffffff");
+		batteryBar.setAttribute("d", "M160 0 L160 52");
+		batteryBar.setAttribute('stroke-width', "2");
+		batteryBar.setAttribute('opacity', "0.25");
+		shiftedContentContainer.appendChild(batteryBar);
+
+		var batteryBar = document.createElementNS(svgNS, "path");
+		batteryBar.setAttribute("stroke", "#ffffff");
+		batteryBar.setAttribute("d", "M180 0 L180 52");
+		batteryBar.setAttribute('stroke-width', "2");
+		batteryBar.setAttribute('opacity', "0.25");
+		shiftedContentContainer.appendChild(batteryBar);
+
+		svg.appendChild(shiftedContentContainer);
+		wrapper.appendChild(svg);
 
 		return wrapper;
 	},
@@ -71,8 +146,11 @@ Module.register("MMM-Tesla",{
 			return;
 		}
 		
-		this.battery_level = data.battery_level;
+		this.battery_level = data.usable_battery_level;
 		this.charging_state = data.charging_state;
+		this.range = data.ideal_battery_range*1.609344;
+
+		generateSVG(this.battery_level);
 	
 		return;
 	},
@@ -104,7 +182,24 @@ Module.register("MMM-Tesla",{
 				this.processDrivestateData(JSON.parse(payload).response);
 				this.updateDom();
     		}
-	} 	
+	},
+
+// 			<svg width="210pt" height="60pt" viewBox="0 0 210 60" version="1.1" xmlns="http://www.w3.org/2000/svg">
+//   <g id="#000000ff" name="background">
+//     <path fill="#000000" opacity="1.00" d="M0 0 L220 0 L220 70 L0 70" />
+//   </g>
+
+//   <g id="#1f6e43ff" transform="translate(5,5)">
+//     <rect width="170" height="50" rx="1" ry="1" style="fill:rgba(63,134,44,0.7);" />
+//     <rect width="200" height="50" rx="2" ry="2" style="fill:rgba(0,0,0,0);stroke-width:1.5;stroke:rgba(255,255,255, 0.25)" />
+
+//     <path stroke="#ffffff" stroke-width="1.5" opacity="0.25" d="M100 0 L100 50" />
+//     <path stroke="#ffffff" stroke-width="1.5" opacity="0.25" d="M120 0 L120 50" />
+//     <path stroke="#ffffff" stroke-width="1.5" opacity="0.25" d="M140 0 L140 50" />
+//     <path stroke="#ffffff" stroke-width="1.5" opacity="0.25" d="M160 0 L160 50" />
+//     <path stroke="#ffffff" stroke-width="1.5" opacity="0.25" d="M180 0 L180 50" />
+//   </g>
+// </svg>
 
 });
 
--- node_helper.js
+++ node_helper.js
@@ -10,87 +10,130 @@ const NodeHelper = require('node_helper');
 var request = require('request');
 var moment = require('moment');
 var accessToken = null;
-module.exports = NodeHelper.create({
 
+function getToken(config) {
+	try {
+		const token = getTokenInternal(config);
+		return token;
+    } catch (err) {
+		console.log(err);
+		return "abc";
+	}
+}
+
+async function getTokenInternal(config) {
+
+	// Set the configuration settings
+	const credentials = {
+		client: {
+			id: config.client_id,
+			secret: config.client_secret
+		},
+		auth: {
+			tokenHost: 'https://owner-api.teslamotors.com',
+			tokenPath: '/oauth/token'
+		  },
+		  http: {
+			headers: { 'User-Agent': 'MMM-Tesla' }
+		}
+	};
+		
+	const oauth2 = require('simple-oauth2').create(credentials);
+
+	const tokenConfig = {
+		email: config.email,
+		password: config.password,
+		grant_type: 'password',
+		client_secret: config.client_secret,
+		client_id: config.client_id
+	};
+
+	try {
+		var tokenObject = await oauth2.ownerPassword.getToken(tokenConfig);
+		return tokenObject.access_token;
+	} catch (error) {
+		console.log('Access Token Error', error.message);
+	}
+}
+
+module.exports = NodeHelper.create({
 
 	start: function() {
 		this.started = false;
 		this.config = null;
 		this.drivestate_data = null;
 		this.charge_data = null;
-		accessToken = null;
 	},
 
-
 	getData: function() {
-		var self = this;	
+		var self = this;
+		var tempToken;
+		
+		const base_data_url = 'https://owner-api.teslamotors.com/api/1/vehicles/' + this.config.vehicle_id;
 
-	// Set the configuration settings
-	const credentials = {
-	  client: {
-	    id: this.config.client_id,
-	    secret: this.config.client_secret
-	  },
-	  auth: {
-	    tokenHost: 'https://owner-api.teslamotors.com/oauth/token'
-	  },
-	  http: {
-	  	headers: { 'User-Agent': 'MMM-Tesla' }
-	  }
-	};
+		if (accessToken === null ) {
+			tempToken = getToken(this.config).then(function(accessToken) {
+				request({
+					url: base_data_url + '/data_request/charge_state',
+					method: 'GET',
+					headers: { 'Authorization': "Bearer " + accessToken, 'Content-type': "application/json; charset=utf-8", 'User-Agent': 'MMM-Tesla' }
+				}, function (error, response, body) {
+					console.log('Polling charge state'); // Print the HTML 
+					if (!error && response.statusCode == 200) {
+						self.charge_data = body;
+						self.sendSocketNotification("CHARGE_DATA", body);
+						request({
+							url: base_data_url + '/data_request/drive_state',
+							method: 'GET',
+							headers: { 'Authorization': "Bearer " + accessToken, 'Content-type': "application/json; charset=utf-8", 'User-Agent': 'MMM-Tesla' }
+						}, function (error, response, body) {
+							console.log('Polling drive state');
+							self.drivestate_data = body;
+							if (!error && response.statusCode == 200) {
+								self.sendSocketNotification("DRIVESTATE_DATA", body);
+							}
+						})
+					} else {
+						console.log(accessToken);
+						console.log('Error: ' + error)
+					}
+				})
+				return accessToken;
+			});
+			accessToken = tempToken;
+		}
+		else {
+			/*
+			accessToken.then(function(accessToken){
+				request({
+					url: base_data_url + '/data_request/charge_state',
+					method: 'GET',
+					headers: { 'Authorization': "Bearer " + accessToken, 'Content-type': "application/json; charset=utf-8", 'User-Agent': 'MMM-Tesla' }
+				}, function (error, response, body) {
+					console.log('Polling charge state'); // Print the HTML 
+					if (!error && response.statusCode == 200) {
+						self.charge_data = body;
+						self.sendSocketNotification("CHARGE_DATA", body);
+						request({
+							url: base_data_url + '/data_request/drive_state',
+							method: 'GET',
+							headers: { 'Authorization': "Bearer " + accessToken, 'Content-type': "application/json; charset=utf-8", 'User-Agent': 'MMM-Tesla' }
+						}, function (error, response, body) {
+							console.log('Polling drive state');
+							self.drivestate_data = body;
+							if (!error && response.statusCode == 200) {
+								self.sendSocketNotification("DRIVESTATE_DATA", body);
+							}
+						})
+					} else {
+						console.log(accessToken);
+						console.log('Error: ' + error)
+					}
+				})
+			}) */
+		}
 
-	// Initialize the OAuth2 Library
-	const oauth2 = require('simple-oauth2').create(credentials);
-	// Get the access token object.
-	const tokenConfig = {
-	  email: this.config.email,
-	  password: this.config.password,
-	  grant_type: 'password'
-	};
-
-	const vehicle_id = this.config.vehicle_id;
-	const base_data_url = 'https://owner-api.teslamotors.com/api/1/vehicles/' + vehicle_id;
-
-	oauth2.ownerPassword.getToken(tokenConfig, (error, result) => {
-		  if (error) {
-		    return console.log('Access Token Error: ', error.message);
-  		  }
-  		  if (!accessToken) {
-  		  	accessToken = oauth2.accessToken.create(result);
-  		  } else if (accessToken.expired()) {
-  		  	// Callbacks
-  				accessToken.refresh((error, result) => {
-    				accessToken = result;
-  				})
-  		  }
-		  console.log (accessToken);
-			request({
-				url: base_data_url + '/data_request/charge_state',
-				method: 'GET',
-				headers: { 'Authorization': "Bearer " + accessToken.token.access_token, 'Content-type': "application/json; charset=utf-8", 'User-Agent': 'MMM-Tesla' }
-			}, function (error, response, body) {
-	  			console.log('body:', body); // Print the HTML 
-				if (!error && response.statusCode == 200) {
-					self.charge_data = body;
-					self.sendSocketNotification("CHARGE_DATA", body);
-				        console.log(accessToken);	
-					request({
-						url: base_data_url + '/data_request/drive_state',
-						method: 'GET',
-						headers: { 'Authorization': "Bearer " + accessToken.token.access_token, 'Content-type': "application/json; charset=utf-8", 'User-Agent': 'MMM-Tesla' }
-					}, function (error, response, body) {
-						console.log('body2:', body);
-						self.drivestate_data = body;
-						if (!error && response.statusCode == 200) {
-							self.sendSocketNotification("DRIVESTATE_DATA", body);
-						}
-					})
-				} else {
-					console.log('Error: ' + error)
-				}
-			})
-		})
-		setTimeout(function() { self.getData(); }, this.config.refreshInterval); 
+		setTimeout(function() { self.getData(); }, this.config.refreshInterval);
 	},
 
 	socketNotificationReceived: function(notification, payload) {
